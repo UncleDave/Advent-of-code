121ffcbfd49b04a36dc4a8dc3696025fd6363e43
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2022-12-06 09:21:42 +0100

    add 2022 day 6

---
+++ 2022/6/1.ts
@@ -0,0 +1,29 @@
+import { promises as fs } from 'fs';
+
+function everyCharIsUnique(chars: string): boolean {
+  const uniqueChars: string[] = [];
+
+  for (let char of chars) {
+    if (uniqueChars.indexOf(char) !== -1)
+      return false;
+
+    uniqueChars.push(char);
+  }
+
+  return true;
+}
+
+(async function () {
+  const fileContent = await fs.readFile('input.txt', { encoding: 'utf-8' });
+  const input = fileContent.replaceAll('\n', '');
+  let firstMarkerIndex = -1;
+
+  for (let i = 3; i < input.length; i++) {
+    if (everyCharIsUnique(input.substring(i - 3, i + 1))) {
+      firstMarkerIndex = i + 1;
+      break;
+    }
+  }
+
+  console.log(firstMarkerIndex);
+})();
