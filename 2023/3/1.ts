50eec3d1c9a122bc7b3303754806745f55cd2f0b
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2023-12-03 19:16:54 +0100

    add 2023 day 3 WIP

---
+++ 2023/3/1.ts
@@ -0,0 +1,103 @@
+import { getLinesFromFile, mapReduceSum } from "../utils";
+
+interface PartNumber {
+  value: number;
+  row: number;
+  columns: number[];
+}
+
+interface Distance {
+  sum: number;
+  x: number;
+  y: number;
+}
+
+function getManhattanDistance(
+  a: [number, number],
+  b: [number, number],
+): Distance {
+  const xDistance = Math.abs(a[0] - b[0]);
+  const yDistance = Math.abs(a[1] - b[1]);
+
+  return {
+    sum: xDistance + yDistance,
+    x: xDistance,
+    y: yDistance,
+  };
+}
+
+(async function () {
+  const input = await getLinesFromFile("./input.txt");
+  const partNumbers: PartNumber[] = [];
+  const symbolPositions: Array<[number, number]> = [];
+
+  for (let rowIndex = 0; rowIndex < input.length; rowIndex++) {
+    const row = input[rowIndex];
+    let currentNumber = "";
+
+    for (let charIndex = 0; charIndex < row.length; charIndex++) {
+      const char = row[charIndex];
+
+      if (char === ".") {
+        if (currentNumber.length > 0) {
+          const spannedColumns: number[] = [];
+
+          for (let i = charIndex; i > currentNumber.length - charIndex; i--) {
+            spannedColumns.push(charIndex - i);
+          }
+
+          partNumbers.push({
+            value: Number(currentNumber),
+            row: rowIndex,
+            columns: spannedColumns,
+          });
+
+          currentNumber = "";
+        }
+
+        continue;
+      }
+
+      if (!isNaN(Number(char))) {
+        currentNumber += char;
+        continue;
+      }
+
+      symbolPositions.push([rowIndex, charIndex]);
+    }
+
+    if (currentNumber.length > 0) {
+      const spannedColumns: number[] = [];
+
+      for (let i = row.length; i > currentNumber.length - row.length; i--) {
+        spannedColumns.push(row.length - i);
+      }
+
+      partNumbers.push({
+        value: Number(currentNumber),
+        row: rowIndex,
+        columns: spannedColumns,
+      });
+    }
+  }
+
+  const validPartNumbers = partNumbers.filter((partNumber) =>
+    partNumber.columns.some((column) =>
+      symbolPositions.some((symbolPosition) => {
+        const distance = getManhattanDistance(
+          [partNumber.row, column],
+          symbolPosition,
+        );
+
+        return Math.max(distance.x, distance.y) === 1;
+      }),
+    ),
+  );
+
+  console.log(
+    validPartNumbers.reduce(
+      mapReduceSum((x) => x.value),
+      0,
+    ),
+  );
+})();
