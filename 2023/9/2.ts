6ce5192bbc4ee6f5247b8a1b924c126f3a62b182
Dave Pringle <davepringle46@yahoo.co.uk>
2023-12-10 23:34:00 +0100

    add 2023 day 9

---
+++ 2023/9/2.ts
@@ -0,0 +1,42 @@
+import { getLinesFromFile, mapReduceSum } from "../utils";
+
+function difference(values: number[]): number[] {
+  const result = [];
+
+  for (let i = 1; i < values.length; i++) {
+    result.push(values[i] - values[i - 1]);
+  }
+
+  return result;
+}
+
+(async function () {
+  const input = await getLinesFromFile("./input.txt");
+  const histories = input.map((x) => x.split(" ").map(Number));
+
+  console.log(
+    histories.reduce(
+      mapReduceSum((history) => {
+        const diffs: Array<number[]> = [];
+
+        do {
+          diffs.push(
+            difference(diffs.length === 0 ? history : diffs[diffs.length - 1]),
+          );
+        } while (diffs[diffs.length - 1].some((x) => x !== 0));
+
+        const historyAndDiffs = [history, ...diffs];
+
+        for (let i = historyAndDiffs.length - 2; i >= 0; i--) {
+          const diffToInterpolate = historyAndDiffs[i];
+          const diff = historyAndDiffs[i + 1];
+
+          diffToInterpolate.unshift(diffToInterpolate[0] - diff[0]);
+        }
+
+        return history[0];
+      }),
+      0,
+    ),
+  );
+})();
