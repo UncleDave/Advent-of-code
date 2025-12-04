a32664a4524807a70e0d793a6584f9d3746e32c5
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2025-12-04 11:24:26 +0100

    add 2025 day 4

---
+++ 2025/4/2.ts
@@ -0,0 +1,89 @@
+import { readFileAsString } from "../utils";
+
+type GridValue = "@" | ".";
+type Position = [number, number];
+
+class Grid {
+  constructor(private readonly values: GridValue[][]) {}
+
+  static fromString(input: string): Grid {
+    const rows = input
+      .split("\n")
+      .map((line) => Array.from(line.trim()) as GridValue[]);
+
+    return new Grid(rows);
+  }
+
+  get paperRolls(): Position[] {
+    const positions: Position[] = [];
+
+    for (let y = 0; y < this.values.length; y++) {
+      for (let x = 0; x < this.values[y].length; x++) {
+        if (this.values[y][x] === "@") {
+          positions.push([x, y]);
+        }
+      }
+    }
+
+    return positions;
+  }
+
+  getAdjacentValues([x, y]: Position): GridValue[] {
+    const adjacentPositions: Position[] = [
+      [x, y - 1], // Up
+      [x + 1, y - 1], // Up Right
+      [x + 1, y], // Right
+      [x + 1, y + 1], // Down Right
+      [x, y + 1], // Down
+      [x - 1, y + 1], // Down Left
+      [x - 1, y], // Left
+      [x - 1, y - 1], // Up Left
+    ];
+
+    const adjacentValues: GridValue[] = [];
+
+    for (const [adjX, adjY] of adjacentPositions) {
+      if (
+        adjY >= 0 &&
+        adjY < this.values.length &&
+        adjX >= 0 &&
+        adjX < this.values[adjY].length
+      ) {
+        adjacentValues.push(this.values[adjY][adjX]);
+      }
+    }
+
+    return adjacentValues;
+  }
+
+  get pickablePaperRolls(): Position[] {
+    return this.paperRolls.filter(
+      (roll) =>
+        this.getAdjacentValues(roll).filter((x) => x === "@").length < 4,
+    );
+  }
+
+  pickPaperRoll([x, y]: Position): void {
+    this.values[y][x] = ".";
+  }
+}
+
+(async function () {
+  const input = await readFileAsString("input.txt");
+  const grid = Grid.fromString(input);
+
+  let pickableRolls: Position[];
+  let totalPicked = 0;
+
+  do {
+    pickableRolls = grid.pickablePaperRolls;
+
+    pickableRolls.forEach((roll) => {
+      grid.pickPaperRoll(roll);
+    });
+
+    totalPicked += pickableRolls.length;
+  } while (pickableRolls.length > 0);
+  
+  console.log(totalPicked);
+})();
