e179a06ba3c187166b3383194cac04fe87388602
Dave Pringle <davepringle46@yahoo.co.uk>
2025-12-05 11:07:05 +0100

    add 2025 day 5

---
+++ 2025/5/1.ts
@@ -0,0 +1,64 @@
+﻿import { readFileAsString } from "../utils";
+
+class Range {
+  constructor(
+    public readonly start: number,
+    public readonly end: number,
+  ) {}
+
+  contains(value: number): boolean {
+    return value >= this.start && value <= this.end;
+  }
+
+  filter(predicate: (value: number) => boolean): number[] {
+    const result: number[] = [];
+
+    for (let i = this.start; i <= this.end; i++) {
+      if (predicate(i)) {
+        result.push(i);
+      }
+    }
+
+    return result;
+  }
+
+  map<T>(mapper: (value: number) => T): T[] {
+    const result: T[] = [];
+
+    for (let i = this.start; i <= this.end; i++) {
+      result.push(mapper(i));
+    }
+
+    return result;
+  }
+}
+
+const inputRegex = /^(?<range>\d+-\d+)|(?<id>\d+)$/gm;
+
+(async function () {
+  const input = await readFileAsString("input.txt");
+  const parsedInput = input.trim().matchAll(inputRegex);
+
+  const { fresh, available } = parsedInput.reduce(
+    ({ fresh, available }, match) => {
+      if (match.groups?.range) {
+        const [start, end] = match.groups.range.split("-").map(Number);
+        fresh.push(new Range(start, end));
+      }
+
+      if (match.groups?.id) {
+        available.push(Number(match.groups.id));
+      }
+
+      return {
+        fresh,
+        available,
+      };
+    },
+    { fresh: [] as Range[], available: [] as number[] },
+  );
+  
+  const availableFreshIngredients = available.filter(x => fresh.some(y => y.contains(x)));
+  
+  console.log(availableFreshIngredients.length);
+})();
---
+++ 2025/5/2.ts
@@ -0,0 +1,44 @@
+﻿import { distinct, mapReduceSum, readFileAsString } from "../utils";
+
+class Range {
+  constructor(
+    public start: number,
+    public end: number,
+  ) {}
+}
+
+const inputRegex = /^(?<range>\d+-\d+)$/gm;
+
+(async function () {
+  const input = await readFileAsString("input.txt");
+  const parsedInput = input.trim().matchAll(inputRegex);
+
+  const fresh = parsedInput.reduce((acc, match) => {
+    if (match.groups?.range) {
+      const [start, end] = match.groups.range.split("-").map(Number);
+      acc.push(new Range(start, end));
+    }
+
+    return acc;
+  }, [] as Range[]);
+
+  fresh.sort((a, b) => a.start - b.start);
+
+  const mergedFresh: Range[] = [];
+
+  for (const range of fresh) {
+    const last = mergedFresh[mergedFresh.length - 1];
+    if (last && range.start <= last.end + 1) {
+      last.end = Math.max(last.end, range.end);
+    } else {
+      mergedFresh.push(new Range(range.start, range.end));
+    }
+  }
+
+  const freshIngredientCount = mergedFresh.reduce(
+    mapReduceSum((x) => x.end - x.start + 1),
+    0,
+  );
+
+  console.log(freshIngredientCount);
+})();
