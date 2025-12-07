9fd195f33cb3a9c2f80e105bb4d02f73d459f233
Dave Pringle <davepringle46@yahoo.co.uk>
2025-12-07 20:51:10 +0100

    add 2025 day 7 part 2

--- 2025/7/2.ts
+++ 2025/7/2.ts
@@ -1,11 +1,9 @@
-import { readFileAsString } from "../utils";
+import { mapReduceSum, readFileAsString } from "../utils";
 
-type GridValue = "S" | "." | "^";
+type GridValue = "S" | "." | "^" | number;
 type Position = [number, number];
 
 class Grid {
-  public timelineCount = 1;
-
   constructor(private readonly values: GridValue[][]) {}
 
   static fromString(input: string): Grid {
@@ -29,35 +27,71 @@ class Grid {
     throw new Error("Start position not found");
   }
 
-  moveBeam([r, c]: Position) {
-    if (r === this.values.length - 1) {
-      return;
+  getTimelineCount(): number {
+    const startPosition = this.startPosition;
+    this.setValue(startPosition, 1);
+
+    for (let r = 1; r < this.values.length; r++) {
+      const row = this.values[r];
+
+      for (let c = 0; c < row.length; c++) {
+        const value = row[c];
+        const valueAbove = this.values[r - 1][c];
+
+        if (value === "^" && Number.isInteger(valueAbove)) {
+          const positionLeft: Position = [r, c - 1];
+          const positionRight: Position = [r, c + 1];
+
+          const valueLeft = this.values[r][c - 1];
+          const valueRight = this.values[r][c + 1];
+
+          if (Number.isInteger(valueLeft)) {
+            this.setValue(
+              positionLeft,
+              (valueAbove as number) + (valueLeft as number),
+            );
+          } else {
+            this.setValue(positionLeft, valueAbove as number);
+          }
+
+          if (Number.isInteger(valueRight)) {
+            this.setValue(
+              positionRight,
+              (valueAbove as number) + (valueRight as number),
+            );
+          } else {
+            this.setValue(positionRight, valueAbove as number);
+          }
+        } else if (Number.isInteger(valueAbove)) {
+          this.setValue([r, c], value === "." ? valueAbove as number : (valueAbove as number) + (value as number));
+        }
+      }
     }
 
-    console.log("Row ", r + 1, " Timelines: ", this.timelineCount);
-
-    const [nextR, nextC] = [r + 1, c];
-    const valueBelow = this.values[nextR][nextC];
-
-    if (valueBelow === "^") {
-      this.timelineCount++;
-
-      const leftPosition: Position = [nextR, nextC - 1];
-      const rightPosition: Position = [nextR, nextC + 1];
-
-      this.moveBeam(leftPosition);
-      this.moveBeam(rightPosition);
-    } else {
-      this.moveBeam([nextR, nextC]);
+    for (let i = 0; i < this.values.length; i++) {
+      console.log(this.values[i].join(" "));
     }
+
+    return (
+      this.values[this.values.length - 1].filter((x) =>
+        Number.isInteger(x),
+      ) as number[]
+    ).reduce(
+      mapReduceSum((x) => x),
+      0,
+    );
+  }
+
+  setValue(position: Position, value: GridValue) {
+    const [r, c] = position;
+    this.values[r][c] = value;
   }
 }
 
 (async function () {
   const input = await readFileAsString("input.txt");
   const grid = Grid.fromString(input);
+  const timelineCount = grid.getTimelineCount();
 
-  grid.moveBeam(grid.startPosition);
-
-  console.log(grid.timelineCount);
+  console.log(timelineCount);
 })();
